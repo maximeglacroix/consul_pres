5bf0f6e7b5fc3f6279fe56cb8b4dbffc13c121a0
Maxime Lacroix <maximeglacroix@gmail.com>
2018-09-26 08:14:36 -0400

    Méthodes_avancées

--- Présentation_1/code_base.R
+++ Présentation_1/code_base.R
@@ -7,6 +7,8 @@ data_csv$continent <- factor(countrycode(sourcevar = data_csv[, "Country Code"],
                                      origin = "iso3c",
                                      destination = "continent"))
 data_csv$Value <- data_csv$Value / 100
+data_csv$Decennie <- ifelse(data_csv$Year<2000,1990,
+                       ifelse(data_csv$Year<2010,2000,2010))
 dat_prop <- data_csv[!is.na(data_csv$continent),]
 dat_prop <- dat_prop[dat_prop$Value>0,]
 hist(dat_prop$Value,main="Histogramme des proportions de femmes au parlement",
--- Présentation_1/models.R
+++ Présentation_1/models.R
@@ -1,16 +1,32 @@
-x = summary(mod1)
-library(sjPlot)
-library(sjmisc)
-library(sjlabelled)
-tab_model(mod1,transform = NULL,
-          show.ci = NULL,
-          show.std="std",
-          show.stat = TRUE)
+library(betareg)
+setwd("C:/Users/Maxime Lacroix/Desktop/consul_pres/Présentation_1")
+source("code_base.R")
+library(lmtest)
+mod1 <- betareg(Value~continent,
+                link = "logit",
+                data=dat_prop)
+
+mod2 <- betareg(Value~continent|continent,
+                link = "logit",
+                link.phi = "log",
+                data=dat_prop)
+mod3 <- betareg(Value~continent|1,
+                link = "log",
+                link.phi = "identity",
+                data=dat_prop)
+
+mod4 <- betatree(Value~continent,
+                 ~Decennie, 
+                 link = "logit",
+                 data=dat_prop,
+                 minsize=100)
+plot(mod4)
+lrtest(mod1,mod2)
+lrtest(mod3,mod2)
+lrtest(mod1,mod3)
+summary(mod3)
+summary(mod2)
 summary(mod1)
 
-mod2 <- betareg(Value~continent|continent,link = ("logit"),data=dat_prop)
-tab_model(mod2,transform = NULL,
-          show.ci = NULL,
-          show.std="std",
-          show.stat = TRUE)
-
+AIC(mod1,mod2,mod3)
+BIC(mod1,mod2,mod3)
